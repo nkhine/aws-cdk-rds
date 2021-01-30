55b876e5b9f84da72919f62ed1bc69baf9cd4173
Norman Khine <norman@khine.net>
2021-01-30 00:15:25 +0000

    add tags

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
-import { App, Construct, Duration, Stack, StackProps } from '@aws-cdk/core';
+import { App, Construct, Duration, CfnOutput, Stack, StackProps, Tags } from '@aws-cdk/core';
 import * as ec2 from '@aws-cdk/aws-ec2';
 import * as rds from '@aws-cdk/aws-rds';
 import * as secrets from '@aws-cdk/aws-secretsmanager';
 const ssm = require('@aws-cdk/aws-ssm');
+// import {AttachmentTargetType, ISecretAttachmentTarget, SecretAttachmentTargetProps, SecretTargetAttachment} from "@aws-cdk/aws-secretsmanager";
 
 export class MwCatalogueStack extends Stack {
+
   constructor(scope: Construct, id: string, props: StackProps = {}) {
     super(scope, id, props);
 
@@ -33,7 +35,7 @@ export class MwCatalogueStack extends Stack {
       stringValue: databaseCredentialsSecret.secretArn,
     });
 
-    new rds.ServerlessCluster(this, 'DBCluster', {
+    const cluster = new rds.ServerlessCluster(this, 'DBCluster', {
       engine: rds.DatabaseClusterEngine.AURORA_POSTGRESQL,
       vpc: vpc,
       parameterGroup: rds.ParameterGroup.fromParameterGroupName(this, 'ParameterGroup', 'default.aurora-postgresql10'),
@@ -43,6 +45,27 @@ export class MwCatalogueStack extends Stack {
         maxCapacity: rds.AuroraCapacityUnit.ACU_16, // default is 16 Aurora capacity units (ACUs)
         autoPause: Duration.minutes(10),
       },
+    });
+
+    Tags.of(cluster).add('Service', 'System');
+    Tags.of(cluster).add('Purpose', 'Operations');
+
+    new CfnOutput(this,'ClusterARN', {
+      description: 'The RDS cluster ARN',
+      value: cluster.clusterArn,
+      exportName: `${this.stackName}-CLUSTER-ARN`
+    });
+
+    new CfnOutput(this,'ClusterId', {
+      description: 'The RDS cluster identifier',
+      value: cluster.clusterIdentifier,
+      exportName: `${this.stackName}-CLUSTER-ID`
+    });
+
+    new CfnOutput(this,'DNSName', {
+      description: 'The connection endpoint for the DB cluster.',
+      value: cluster.clusterEndpoint.hostname,
+      exportName: `${this.stackName}-CLUSTER-DNS`
     });
 
   }
